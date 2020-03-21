ea8f472105e0d31f2a51285caa503414be02e2ca
CHAITANYA <Chaitanyas0101@gmail.com>
2020-03-22 00:13:37 +0530

    added tesseract to extract text from image.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.image_to_string('')
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+
+image = Image.open('image_in_project')
+text = pytesseract.image_to_string(image)
+print(text)
